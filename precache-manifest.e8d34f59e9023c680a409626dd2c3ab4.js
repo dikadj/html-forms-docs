9de57b8b28c0eee921e22385925b7c7709249cd8
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2020-12-20 15:31:13 +0700

    Updates

---
+++ precache-manifest.e8d34f59e9023c680a409626dd2c3ab4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "93f42760d283ddf6f329",
+    "url": "/html-forms-docs/static/js/main.3b9e4bc6.chunk.js"
+  },
+  {
+    "revision": "7a97c6b75ea47d7ced62",
+    "url": "/html-forms-docs/static/js/runtime~main.00fdfbd4.js"
+  },
+  {
+    "revision": "918994b293c26999f84c",
+    "url": "/html-forms-docs/static/js/2.509fda2e.chunk.js"
+  },
+  {
+    "revision": "71e57c5595bf29c131a9261c85cbc7b0",
+    "url": "/html-forms-docs/index.html"
+  }
+];
